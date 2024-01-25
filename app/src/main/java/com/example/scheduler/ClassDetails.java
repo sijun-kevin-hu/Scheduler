9cd6cfd7db3f5fe0653dd133b0b491a106200e7e
Kevin Hu <22kevinhu@gmail.com>
2024-01-25 15:03:19 -0500

    added ClassDetails class

---
+++ app/src/main/java/com/example/scheduler/ClassDetails.java
@@ -0,0 +1,25 @@
+package com.example.scheduler;
+
+import java.util.ArrayList;
+
+public class ClassDetails<T> implements Modifiable<T> {
+    private ArrayList<T> list = new ArrayList<>();
+    public ClassDetails () {
+
+    }
+
+    @Override
+    public void add(T data) {
+
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+
+    @Override
+    public void edit(T data, int index) {
+
+    }
+}
