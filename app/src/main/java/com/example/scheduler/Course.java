5d73a7dd3fcab6858678c24167470d21680302be
Kevin Hu <22kevinhu@gmail.com>
2024-01-25 22:05:53 -0500

    updated Course.java

--- app/src/main/java/com/example/scheduler/Course.java
+++ app/src/main/java/com/example/scheduler/Course.java
@@ -1,13 +1,16 @@
 package com.example.scheduler;
 
+/**
+ * This class represents a Course object
+ */
 public class Course<T> implements Modifiable<T> {
-    private T[] backingArray;
+    private T[] assignments;
     static final int MAX_CAPACITY = 100;
     private int size;
     private String name;
 
     public Course(String name) {
-        backingArray = (T[]) new Object[MAX_CAPACITY];
+        assignments = (T[]) new Object[MAX_CAPACITY];
         size = 0;
         this.name = name;
     }
@@ -16,15 +19,15 @@ public class Course<T> implements Modifiable<T> {
     public void add(T data) {
         checkData(data);
         int i = 0;
-        while (backingArray[i] != null) {i++;}
-        backingArray[i] = data;
+        while (assignments[i] != null) {i++;}
+        assignments[i] = data;
         size++;
     }
 
     @Override
     public void delete(int index) {
         checkIndex(index);
-        backingArray[index] = null;
+        assignments[index] = null;
         size--;
     }
 
@@ -32,7 +35,7 @@ public class Course<T> implements Modifiable<T> {
     public void edit(T data, int index) {
         checkData(data);
         checkIndex(index);
-        backingArray[index] = data;
+        assignments[index] = data;
     }
 
     private void checkData(T data) {
@@ -44,7 +47,7 @@ public class Course<T> implements Modifiable<T> {
     private void checkIndex(int index) {
         if (index < 0 || index >= MAX_CAPACITY) {
             throw new IndexOutOfBoundsException("The inputted index is out of bounds.");
-        } else if (backingArray[index] == null) {
+        } else if (assignments[index] == null) {
             throw new NullPointerException("The array is empty at that index.");
         }
     }
