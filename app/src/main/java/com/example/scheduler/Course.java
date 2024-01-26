09e0407c90d8a886a75abaf8a81e12088bf302eb
Kevin Hu <22kevinhu@gmail.com>
2024-01-25 22:15:20 -0500

    updated Course.java

--- app/src/main/java/com/example/scheduler/Course.java
+++ app/src/main/java/com/example/scheduler/Course.java
@@ -9,21 +9,33 @@ public class Course<T> implements Modifiable<T> {
     private int size;
     private String name;
 
+    /**
+     * This constructor initializes assignments and sets name variable.
+     * @param name a name for the Course
+     */
     public Course(String name) {
         assignments = (T[]) new Object[MAX_CAPACITY];
         size = 0;
         this.name = name;
     }
 
+    /**
+     * This method adds an assignment to Course
+     * @param assignment The assignment
+     */
     @Override
-    public void add(T data) {
-        checkData(data);
+    public void add(T assignment) {
+        checkData(assignment);
         int i = 0;
         while (assignments[i] != null) {i++;}
-        assignments[i] = data;
+        assignments[i] = assignment;
         size++;
     }
 
+    /**
+     * This method deletes assignment from index
+     * @param index an index
+     */
     @Override
     public void delete(int index) {
         checkIndex(index);
@@ -31,19 +43,48 @@ public class Course<T> implements Modifiable<T> {
         size--;
     }
 
+    /**
+     * This method edits the data at an index
+     * @param assignment an assignment object
+     * @param index an index
+     */
     @Override
-    public void edit(T data, int index) {
-        checkData(data);
+    public void edit(T assignment, int index) {
+        checkData(assignment);
         checkIndex(index);
-        assignments[index] = data;
+        assignments[index] = assignment;
     }
 
+    /**
+     * A getter method for name variable
+     * @return a String name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * A setter method for name variable
+     * @param name a String for name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Checks if data is empty
+     * @param data an object T
+     */
     private void checkData(T data) {
         if (data == null) {
             throw new NullPointerException("The inputted data is empty.");
         }
     }
 
+    /**
+     * Checks if index is out of bounds or empty
+     * @param index an int index
+     */
     private void checkIndex(int index) {
         if (index < 0 || index >= MAX_CAPACITY) {
             throw new IndexOutOfBoundsException("The inputted index is out of bounds.");
