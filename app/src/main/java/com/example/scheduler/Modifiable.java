da4cfa9de3194e7efe5caaedda0c94644608ffc2
Kevin Hu <22kevinhu@gmail.com>
2024-01-28 18:49:58 -0500

    updated Modificable.java and Course.java

--- app/src/main/java/com/example/scheduler/Modifiable.java
+++ app/src/main/java/com/example/scheduler/Modifiable.java
@@ -1,9 +1,22 @@
 package com.example.scheduler;
 
 public interface Modifiable<T> {
+    /**
+     * This method adds an assignment to Course
+     * @param data The assignment
+     */
     public void add(T data);
 
+    /**
+     * This method deletes assignment from index
+     * @param index an index
+     */
     public void delete(int index);
 
+    /**
+     * This method edits the data at an index
+     * @param data an assignment object
+     * @param index an index
+     */
     public void edit(T data, int index);
 }
