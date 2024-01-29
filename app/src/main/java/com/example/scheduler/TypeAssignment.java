14fa6c36e60c40bcb346564737ea3962f079bb0c
Matt Erickson <mterickson404@gmail.com>
2024-01-28 19:25:53 -0500

    added the typeAssignment enum

---
+++ app/src/main/java/com/example/scheduler/TypeAssignment.java
@@ -0,0 +1,11 @@
+package com.example.scheduler;
+
+/**
+ * This class outlines the enum for the different types of assignments.
+ */
+public enum TypeAssignment {
+    EXAM,
+    HOMEWORK,
+    QUIZZES,
+    PROJECTS;
+}
