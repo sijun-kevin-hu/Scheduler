f93652d1ca1b7712776571b0843f71f53dafdb71
Kevin Hu <22kevinhu@gmail.com>
2024-01-28 20:20:37 -0500

    deleted methods from Course.java

--- app/src/main/java/com/example/scheduler/Course.java
+++ app/src/main/java/com/example/scheduler/Course.java
@@ -4,8 +4,6 @@ package com.example.scheduler;
  * This class represents a Course object
  */
 public class Course<T> implements Modifiable<T> {
-    private T[] assignments;
-    private int size;
     private String name;
 
     /**
@@ -13,8 +11,6 @@ public class Course<T> implements Modifiable<T> {
      * @param name a name for the Course
      */
     public Course(String name) {
-        assignments = (T[]) new Object[Catalog.MAX_CAPACITY];
-        size = 0;
         this.name = name;
     }
 
@@ -22,23 +18,17 @@ public class Course<T> implements Modifiable<T> {
     public void add(T data) {
         checkData(data);
         int i = 0;
-        while (assignments[i] != null) {i++;}
-        assignments[i] = data;
-        size++;
     }
 
     @Override
     public void delete(int index) {
         checkIndex(index);
-        assignments[index] = null;
-        size--;
     }
 
     @Override
     public void edit(T data, int index) {
         checkData(data);
         checkIndex(index);
-        assignments[index] = data;
     }
 
     /**
@@ -56,11 +46,7 @@ public class Course<T> implements Modifiable<T> {
      * @param index an int index
      */
     private void checkIndex(int index) {
-        if (index < 0 || index >= Catalog.MAX_CAPACITY) {
-            throw new IndexOutOfBoundsException("The inputted index is out of bounds.");
-        } else if (assignments[index] == null) {
-            throw new NullPointerException("The array is empty at that index.");
-        }
+
     }
 
     /**
@@ -88,9 +74,5 @@ public class Course<T> implements Modifiable<T> {
     public void setName(String name) {
         checkName(name);
         this.name = name;
-    }
-
-    public T[] getArray() {
-        return assignments;
     }
 }
